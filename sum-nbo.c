a6d08090de5fd166692ccc2cbe67646d0d63ba41
lhywk <lhywkd22@gmail.com>
2026-06-28 20:04:01 +0900

    Update sum-nbo.c

---
+++ sum-nbo.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+uint32_t myntohl(uint32_t n) {
+	uint32_t n1 = (n & 0xFF000000) >> 24;
+	uint32_t n2 = (n & 0x00FF0000) >> 8;
+	uint32_t n3 = (n & 0x0000FF00) << 8;
+	uint32_t n4 = (n & 0x000000FF) << 24;
+	return n1 | n2 | n3 | n4;
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		fprintf(stderr, "syntax : %s <file1> [<file2>...]\n", argv[0]);
+		fprintf(stderr, "sample : %s a.bin b.bin c.bin\n", argv[0]);
+		return 1;
+	}
+
+	uint32_t values[argc - 1];
+	uint32_t sum = 0;
+
+	for (int i=1;i<argc;i++) {
+		FILE *fp = fopen(argv[i], "rb");
+		if (fp == NULL) {
+			fprintf(stderr, "Error: cannot open file %s \n", argv[i]);
+			return 1;
+		}
+
+		uint32_t raw;
+		size_t n = fread(&raw, sizeof(raw), 1, fp);
+		if (n != 1) {
+			fprintf(stderr, "Error: cannot read 4 bytes from %s\n", argv[i]);
+			fclose(fp);
+			return 1;
+		}
+
+		fclose(fp);
+
+		uint32_t value = myntohl(raw);
+		values[i-1] = value;
+		sum = sum + value;
+	}
+
+	for (int i=0;i<argc-1;i++) {
+		printf("%d(0x%08x)", values[i], values[i]);
+		if (i<argc-2)
+			printf(" + ");
+	}
+	printf(" = %d(0x%08x)\n", sum, sum);
+
+	return 0;
+}
+
